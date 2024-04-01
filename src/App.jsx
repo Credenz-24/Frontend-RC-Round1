b25f500f10e5168cbea52cd88af06dfc1306dcb5
Divya Pardeshi <divya.nsk22@gmail.com>
2024-04-01 18:27:43 +0530

    added rc result page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import React from 'react';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import  Login  from './pages/Login';
-import Instruction from './pages/Instruction';
-import QuestionMcq from './pages/QuestionMcq';
-import Leaderboard from './pages/Leaderboard';
-import Result from './pages/Result';
-import OurTeam from './pages/OurTeam';
-import NavBar from "./components/Navbar";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-      <>
-      <Route path="/" element={<Login />} />
-      <Route path="instruction" element={<Instruction/>} />
-      <Route path="question" element={<QuestionMcq/>} />
-      <Route path="leaderboard" element={<Leaderboard/>} />
-      <Route path="result" element={<Result/>} />
-      <Route path="Ourteam" element={<OurTeam/>} />
-      </>     
-  
-  )
-)
-
-function App({routes}) {
-
+import './App.css';
+import Home from './components/Home';
+import Nav from './components/Nav';
+function App() {
   return (
-    <div className='min-w-[100vw] min-h-[100vh]'>
-      <NavBar />
-      <RouterProvider router={router}/>
+    <div className="App ">
+      <Nav/>
+      <Home/>
     </div>
+    
   );
 }
 
 export default App;
-
-
-
